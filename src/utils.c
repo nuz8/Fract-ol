4e57279bf4b7b2eac9c2e83385d244b5297d9275
pamatya <hydroweb.pranujamatya@gmail.com>
2024-07-26 05:45:22 +0200

    Added frexit function with mlx_terminate

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: pamatya <pamatya@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 00:28:21 by pamatya           #+#    #+#             */
-/*   Updated: 2024/07/26 05:41:47 by pamatya          ###   ########.fr       */
+/*   Updated: 2024/07/26 05:44:48 by pamatya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	printf_cmplx(t_cmplx z);
 
 void	frexit(char *str, t_fractal fr, int ec)
 {
-	
+	perror(str);
+	mlx_terminate(fr.mlx);
+	exit(ec);
 }
 
 
